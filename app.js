ae44748b734190611d0629992a989a17071f33c9
Aiden <aidngonz@gmail.com>
2024-07-16 20:39:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const header = createLayer('header', 10);
 const about = createLayer('about', 20);
 const more = createLayer('more', 30);
 
-createElementAndAppend(header.element, 'h1', 'BreezeJS');
-createElementAndAppend(header.element, 'h3', 'Breeze.js is a lightweight JavaScript framework designed for simplified web development, focusing on DOM manipulation and state management.');
+createElementAndAppend(header.element, 'h1', {}, 'BreezeJS');
+createElementAndAppend(header.element, 'h3', {}, 'Breeze.js is a lightweight JavaScript framework designed for simplified web development, focusing on DOM manipulation and state management.');
