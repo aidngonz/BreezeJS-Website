a7a5b796db77d749802b8d4375655bf463489218
Aiden <aidngonz@gmail.com>
2024-07-17 11:38:29 -0400

    Add about section and styles

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ const more = createLayer('more', 30);
 
 createElementAndAppend(header.element, 'h1', {classListAdd: 'bigHeader'}, 'BreezeJS');
 createElementAndAppend(header.element, 'h3', {classListAdd: 'sub'}, 'Breeze.js is a lightweight JavaScript framework designed for simplified web development, focusing on DOM manipulation and state management.');
+
+createElementAndAppend(about.element, 'h3', {classListAdd: 'sub'}, 'With a minimalist approach, it offers essential functionalities such as creating DOM elements (newElement), managing document layers (Layer), handling application state (State), create forms and collect form data (Form), and utility functions (Helpers) to streamline development tasks.')
