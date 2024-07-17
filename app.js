df9db3d306c6ebfad6e0767beb116b28aa803746
Aiden <aidngonz@gmail.com>
2024-07-16 20:38:29 -0400

    Add header elements

--- app.js
+++ app.js
@@ -2,29 +2,9 @@ import { createLayer, createState, createForm } from './node_modules/breezejs-we
 import { appendElementWithListener, createElementAndAppend } from './node_modules/breezejs-web/src/helpers.js';
 
 // Layers:
-const layer1 = createLayer('layer1', 10);
-const layer2 = createLayer('layer2', 20);
-const layer3 = createLayer('layer3', 30);
+const header = createLayer('header', 10);
+const about = createLayer('about', 20);
+const more = createLayer('more', 30);
 
-// Elements:
-createElementAndAppend(layer1.element, 'h1', { classListAdd: 'blue' }, 'Hello World in Layer');
-createElementAndAppend(layer1.element, 'p', {}, 'This is a paragraph in layer 1.');
-
-// Buttons and Reactivity:
-const buttonState = createState({ count: 0 });
-
-let button = appendElementWithListener(layer2.element, 'button', {}, 'click', () => {
-    buttonState.setState({ count: buttonState.getState().count + 1 });
-});
-
-button.textContent = `Clicked ${buttonState.getState().count} times`;
-
-buttonState.addListener((state) => {
-    button.textContent = `Clicked ${state.count} times`;
-});
-
-// Form:
-const form = createForm('form', layer3);
-form.addInput('firstInput', 'placeholder');
-form.addSubmit('submit', 'Submit');
-console.log(form.getDataOnSubmit());
+createElementAndAppend(header.element, 'h1', 'BreezeJS');
+createElementAndAppend(header.element, 'h3', 'Breeze.js is a lightweight JavaScript framework designed for simplified web development, focusing on DOM manipulation and state management.');
